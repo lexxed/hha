2fa5a038fdfabc5b4af36a7a5604b23f1189901e
lexxed <lexx.tham@gmail.com>
2017-03-22 11:15:47 +0000

    update cust_new_order

--- config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'brandName' => 'HHA Royal Jelly Paper',
-    'supportEmail' => 'help@hhajellypaper.com ',
-    'domainUrl' => 'hhajellypaper.com ',
-    'footercopyright' => 'Hha Royal Jelly Paper Co.',
-];
--- resources/views/emails/cust_new_order.blade.php
+++ resources/views/emails/cust_new_order.blade.php
@@ -1,15 +1,19 @@
 Hi {{ $customerinformation->name }},
-<br>
+<br><br>
 Thank you for your order.
 <br>
 Your orders are :<br>
 @foreach ($cartitem as $item)
     {{ $item->qty }} x {{ $item->name }}<br>
 @endforeach
+<br>
+Orders are usually shipped within 2 working days.
 <br><br>
-This is your order id :
-<br>
-You can track your orders here.
-<br>
-Thank you,
-Sarah
+You will be emailed the tracking number as soon as it is shipped.
+<br><br>
+You can track your orders at :<br>
+http://hhajellypaper.com/track-order
+<br><br>
+Thank you,<br>
+Sarah<br>
+{{ env('MAIL_FROM_ADDRESS') }}
